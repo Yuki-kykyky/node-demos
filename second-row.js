c77f370dea172c971e6813300e513ecc4c1ca781
yuki <kyoiii0219@163.com>
2025-01-21 16:43:21 +0800

    init package.json

--- second-row.js
+++ second-row.js
@@ -6,10 +6,10 @@ const server = http.createServer((req, res) => {
     res.setHeader('Content-Type', 'text/html');
     res.write('<html lang="en">');
     res.write('<head><title>hello from Node.js</title></head>');
-    res.write('<body><h1>Hello, world! this is some words from Node.js</h1></body>');
+    res.write('<body><h1>Hello, world! i have plan to learn more tech to make myself more clever!</h1></body>');
     res.write('</html>');
     res.end();
-    process.exit();
+    // process.exit();
 });
 
 server.listen(3000);

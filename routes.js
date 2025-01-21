5b70d71138690e465ad22655fe75ca447cf1a700
yuki <kyoiii0219@163.com>
2025-01-21 16:27:46 +0800

    extract routes, use module export

---
+++ routes.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+
+const requestHandler = (req, res) => {
+    const url = req.url;
+    res.setHeader('Content-Type', 'text/html');
+    if (url === '/') {
+        res.write('<html lang="en">');
+        res.write('<head><title>there is a test</title></head>');
+        res.write('<body><form action="/message" method="post">' +
+            '<input type="text" name="test-context"><button type="submit">send</button> ' +
+            '</form></body>');
+        res.write('</html>');
+        return res.end();
+    }
+    if (url === '/message' && req.method === 'POST') {
+        const body = [];
+        req.on('data', (chunk) => {
+            console.log(chunk);
+            body.push(chunk);
+        });
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            console.log(parsedBody)
+            const message = parsedBody.split('=')[1]
+            fs.writeFileSync('message.txt', message)
+            res.statusCode = 302;
+            res.setHeader('Location', '/');
+            return res.end();
+        })
+    }
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html lang="en">');
+    res.write('<head><title>hello from Node.js</title></head>');
+    res.write('<body><h1>Hello, world! this is some words from Node.js</h1></body>');
+    res.write('</html>');
+    res.end();
+    // if you want to keep the server running, remove the process.exit() call
+    // process.exit();
+}
+
+module.exports.handler = requestHandler;
+module.exports.someText = 'some hard coded text';
--- third-row.js
+++ third-row.js
@@ -1,41 +1,6 @@
 const http = require('http');
-const fs = require('fs');
-const server = http.createServer((req, res) => {
-    const url = req.url;
-    res.setHeader('Content-Type', 'text/html');
-    if (url === '/') {
-        res.write('<html lang="en">');
-        res.write('<head><title>there is a test</title></head>');
-        res.write('<body><form action="/message" method="post">' +
-            '<input type="text" name="test-context"><button type="submit">send</button> ' +
-            '</form></body>');
-        res.write('</html>');
-        return res.end();
-    }
-    if (url === '/message' && req.method === 'POST') {
-        const body = [];
-        req.on('data', (chunk) => {
-            console.log(chunk);
-            body.push(chunk);
-        });
-        req.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString();
-            console.log(parsedBody)
-            const message = parsedBody.split('=')[1]
-            fs.writeFileSync('message.txt', message)
-            res.statusCode = 302;
-            res.setHeader('Location', '/');
-            return res.end();
-        })
-    }
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html lang="en">');
-    res.write('<head><title>hello from Node.js</title></head>');
-    res.write('<body><h1>Hello, world! this is some words from Node.js</h1></body>');
-    res.write('</html>');
-    res.end();
-    // if you want to keep the server running, remove the process.exit() call
-    // process.exit();
-});
+const routes = require('./routes');
+
+const server = http.createServer(routes.handler);
 
 server.listen(8008);

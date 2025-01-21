481d07ef7e6f8a459e886cd4ec23ed906340dbdb
yuki <kyoiii0219@163.com>
2025-01-21 16:16:54 +0800

    basic understanding of node.js

--- second-row.js
+++ second-row.js
@@ -1,7 +1,15 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log('Request received');
+    console.log('Request received',
+        req.url, req.method, req.headers);
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html lang="en">');
+    res.write('<head><title>hello from Node.js</title></head>');
+    res.write('<body><h1>Hello, world! this is some words from Node.js</h1></body>');
+    res.write('</html>');
+    res.end();
+    process.exit();
 });
 
 server.listen(3000);

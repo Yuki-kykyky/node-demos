10633d278e2e77c0f0cb806f04e09ebabf11643f
yuki <kyoiii0219@163.com>
2025-01-22 14:14:17 +0800

    import views

--- routes/admin.js
+++ routes/admin.js
@@ -1,19 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/add-note', (req, res, next) => {
-    console.log('this is the first middleware');
-    res.send('<form action="/admin/add-note" method="post"><input type="text" name="note"><button type="submit">Add Note</button></form>');
+router.get('/product', (req, res, next) => {
+    res.send('<form action="/admin/product" method="post"><input type="text" name="note"><button type="submit">Add Note</button></form>');
 });
 
-router.post('/add-note', (req, res, next) => {
-    console.log('this is the second middleware');
-    res.send('<h1>Added Note</h1>');
+router.post('/product', (req, res, next) => {
+    res.send('<h1>Added Product</h1>');
     console.log(req.body);
 })
 
 router.get('/', (req, res, next) => {
-    console.log('this is the third middleware');
     res.send('<h1>Empty here</h1>');
 })
 

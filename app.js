cb8c938812b25c227f7f4ced07a2656f40531390
yuki <kyoiii0219@163.com>
2025-01-22 14:24:43 +0800

    extract not-found.html

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const express = require('express');
+const path = require('path');
 const bodyParser = require('body-parser');
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: false}));
 
 const adminRoutes = require('./routes/admin');
-app.use('/admin',adminRoutes);
+app.use('/admin', adminRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).send('<h1>Page not found</h1>');
+    res.status(404).sendFile(path.join(__dirname, 'views', 'not-found.html'));
 });
 
 app.listen(8008)

0570abbc56468e9d64da7c8dc5166f9b3917c658
yuki <kyoiii0219@163.com>
2025-01-22 11:27:04 +0800

    import body-parser

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('In the middleware');
-    next();
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/add-note', (req, res, next) => {
+    console.log('this is the first middleware');
+    res.send('<form action="/hello" method="post"><input type="text" name="note"><button type="submit">Add Note</button></form>');
 });
 
-app.use((req, res, next) => {
-    console.log('this is the next middleware');
-    res.send('<h1>Hello from express</h1>');
+app.use('/hello', (req, res, next) => {
+    console.log('this is the second middleware');
+    res.send('<h1>Hello</h1>');
+    console.log(req.body);
+
+})
+app.use('/', (req, res, next) => {
+    console.log('this is the third middleware');
+    res.send('<h1>Empty here</h1>');
 })
 
 app.listen(8008)

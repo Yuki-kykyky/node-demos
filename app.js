0f8cded71cd8713eadccad4791e983bd780a9d27
yuki <kyoiii0219@163.com>
2025-01-22 11:54:56 +0800

    update express.route

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
+app.use(bodyParser.urlencoded({extended: false}));
 
 const adminRoutes = require('./routes/admin');
-app.use(adminRoutes);
+app.use('/admin',adminRoutes);
 
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found</h1>');
+});
 
 app.listen(8008)
--- routes/admin.js
+++ routes/admin.js
@@ -1,15 +1,15 @@
 const express = require('express');
-
 const router = express.Router();
 
 router.get('/add-note', (req, res, next) => {
     console.log('this is the first middleware');
-    res.send('<form action="/hello" method="post"><input type="text" name="note"><button type="submit">Add Note</button></form>');
+    res.send('<form action="/admin/add-note" method="post"><input type="text" name="note"><button type="submit">Add Note</button></form>');
 });
 
-router.post('/hello', (req, res, next) => {
+router.post('/add-note', (req, res, next) => {
     console.log('this is the second middleware');
-    res.redirect('/');
+    res.send('<h1>Added Note</h1>');
+    console.log(req.body);
 })
 
 router.get('/', (req, res, next) => {
